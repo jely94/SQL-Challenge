bb4ade75ac72604a259cfebf77c889983215ccf5
Jeff Lyng <jely94@icloud.com>
2020-10-05 19:42:45 -0500

    Added code

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,21 +1,13 @@
 -- Create the tables for each csv file
+DROP TABLE departments;
 CREATE TABLE departments (
   dept_no VARCHAR(5) NOT NULL PRIMARY KEY,
   dept_name VARCHAR(64)	
 );
 
-CREATE TABLE dept_emp (
-  emp_no int NOT NULL,
-  dept_no VARCHAR(5) NOT NULL
-);
-
-CREATE TABLE dept_manager (
-  dept_no VARCHAR(5) NOT NULL,
-  emp_no INT NOT NULL
-);
-
+DROP TABLE employees;
 CREATE TABLE employees (
-  emp_no int NOT NULL,
+  emp_no int NOT NULL PRIMARY KEY,
   emp_title_id VARCHAR(5),
   birth_date VARCHAR(10),
   first_name VARCHAR(64),
@@ -24,13 +16,32 @@ CREATE TABLE employees (
   hire_date VARCHAR(10)
 );
 
-CREATE TABLE salaries (
+DROP TABLE dept_emp;
+CREATE TABLE dept_emp (
   emp_no int NOT NULL,
-  salary INT
+  dept_no VARCHAR(5) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
+DROP TABLE dept_manager;
+CREATE TABLE dept_manager (
+  dept_no VARCHAR(5) NOT NULL,
+  emp_no INT NOT NULL PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+DROP TABLE salaries;
+CREATE TABLE salaries (
+  emp_no int NOT NULL PRIMARY KEY,
+  salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+DROP TABLE titles;
 CREATE TABLE titles (
-  title_id VARCHAR(5) NOT NULL,
+  title_id VARCHAR(5) NOT NULL PRIMARY KEY,
   title VARCHAR(64)
 );
 
